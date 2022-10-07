c258f19d28b651d5fbf245bcb55a1bd1f8fe121e
colorthoro <dream.0112@qq.com>
2022-10-08 03:30:10 +0800

    pinia state 持久化存储，由于插件使用了stringify，无法支持Map类型的持久化存储

--- client/magic_v/src/main.js
+++ client/magic_v/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App).use(createPinia().use(piniaPluginPersistedstate)).mount('#app')
