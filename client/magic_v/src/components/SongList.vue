f756f5b147179db56c2c42e988acc22f801e4c33
colorthoro <dream.0112@qq.com>
2022-10-07 17:32:15 +0800

    getAllSongsFromCloud

--- client/magic_v/src/components/SongList.vue
+++ client/magic_v/src/components/SongList.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
         <div class="container">
-
+            <div class="control" @click="getAllSongsFromCloud">
+                点我获取全部云音乐
+            </div>
+            <div class="all">
+                <div class="song" v-for="song in songLists.all.list" :key="song.file_id">
+                    {{song.name}}
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import {mapState, mapActions} from 'vuex'
+
 export default {
     name:'SongList',
     data(){
@@ -14,8 +23,11 @@ export default {
             
         }
     },
+    computed: {
+        ...mapState(['songLists']),
+    },
     methods: {
-        
+        ...mapActions(['getAllSongsFromCloud']),
     },
 }
 </script>
--- client/magic_v/src/store/index.js
+++ client/magic_v/src/store/index.js
@@ -1,18 +1,43 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 const store = createStore({
     state: {
         songLists: {
-            all: [],
-            lost: [],
-            bin: [],
+            all: { list: [], ids_set: new Set() },
+            lost: { list: [], ids_set: new Set() },
+            bin: { list: [], ids_set: new Set() },
         }
     },
     mutations: {
-        
+        /**
+         * 
+         * @param {array} songs
+         * @param {string} songs[].file_id
+         * @param {string} listName
+         */
+        putIntoList(state, { songs, listName }) {
+            let targetList = state.songLists[listName];
+            if (!targetList) {
+                console.error('list not found: ' + listName);
+                return 0;
+            }
+            console.log(songs)
+            songs = songs.filter(song => {
+                if (targetList.ids_set.has(song.file_id)) return false;
+                targetList.ids_set.add(song.file_id);
+                return true;
+            })
+            console.log(songs)
+            targetList.list = targetList.list.concat(songs);
+            return songs.length;
+        }
     },
     actions: {
-
+        async getAllSongsFromCloud(context) {
+            let res = await axios.get('api/scanMusic');
+            context.commit('putIntoList', { songs: res.data, listName: 'all' });
+        }
     }
 })
 
