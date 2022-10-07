7ba5e5f3c1f6c97d7b91b5349196d1ac04d198f2
colorthoro <dream.0112@qq.com>
2022-10-07 14:27:20 +0800

    创建基本store

---
+++ client/magic_v/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state: {
+        songLists: {
+            all: [],
+            lost: [],
+            bin: [],
+        }
+    },
+    mutations: {
+        
+    },
+    actions: {
+
+    }
+})
+
+export default store;
