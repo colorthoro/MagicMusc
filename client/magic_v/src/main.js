cd0724d82ea71fe6f2a8e2a60d85ddc2e020b874
colorthoro <dream.0112@qq.com>
2022-10-08 19:52:00 +0800

    安装vue-cookies

--- client/magic_v/src/main.js
+++ client/magic_v/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import VueCookies from 'vue-cookies'
 
-createApp(App).use(createPinia().use(piniaPluginPersistedstate)).mount('#app')
+createApp(App)
+    .use(createPinia().use(piniaPluginPersistedstate))
+    .use(VueCookies)
+    .mount('#app')
