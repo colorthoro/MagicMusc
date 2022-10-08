f8fac88da80cf5f58b0dbdc92f4f925759969381
colorthoro <dream.0112@qq.com>
2022-10-08 19:54:41 +0800

    server：简陋版登录、注册接口

--- server_py/server.py
+++ server_py/server.py
@@ -1,20 +1,57 @@
 from queue import Queue
 from time import sleep
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, session
 from aligo import Aligo
 import re
 import json
+import string
+import random, time
 
-app = Flask(__name__,)
+app = Flask(__name__)
 ali = Aligo()
 
 musicPattern = re.compile(r'(.+\.(mp3|avi))', re.I)
+with open('secret.json', 'r', encoding='utf-8') as f:
+    secret = json.load(f)
+
 
 
 @app.route('/')
 def index():
     return 'hi'
 
+@app.route('/register', methods=['post'])
+def register():
+    user_id = request.json['user_id']
+    sp = request.json['sp']
+    if user_id in secret:
+        return 'id已经存在'
+    secret[user_id]={
+        'id': user_id,
+        'sp': sp,
+        'token':{
+            'code':'',
+            'expired': 0,
+        }
+    }
+    return 'ok'
+
+@app.route('/login', methods=['post'])
+def login():
+    user_id = request.json['user_id']
+    sp = request.json['sp']
+    if user_id not in secret:
+        return '请先注册'
+    if secret[user_id]['sp'] != sp:
+        return '密码错误'
+    code = ''.join(random.sample(string.ascii_letters+string.digits, 32))
+    secret[user_id]['token']={
+        'code': code,
+        'expired': time.time() + 60*60*24*3,
+    }
+    with open('secret.json', 'w', encoding='utf-8') as f:
+        json.dump(secret, f)
+    return jsonify(secret[user_id]['token'])
 
 @app.route('/scanMusic')
 def scanMuic():
