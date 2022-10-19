e0c4a212ca1f456e7e2d30654369d119656d65cf
colorthoro <dream.0112@qq.com>
2022-10-19 16:23:04 +0800

    酷狗音乐api，等待使用

--- client/magic_v/src/tools/api.js
+++ client/magic_v/src/tools/api.js
@@ -32,6 +32,29 @@ export function apiGetLyric(id) {
         params: { id }
     });
 }
+
+/*************************下面是酷狗音乐API************************/
+export function apiKugouSongsInfo(keyword) {  // TODO: waiting for using
+    return axios({
+        method: 'get',
+        url: 'http://msearchcdn.kugou.com/api/v3/search/song',
+        params: {
+            showtype: 14, highlight: em, pagesize: 30, tag_aggr: 1, tagtype: '全部',
+            plat: 0, sver: 5,
+            keyword,
+            correct: 1, api_ver: 1, version: 9108, page: 1, area_code: 1, tag: 1, with_res_tag: 1
+        },
+    });
+}
+export function apiKugouLyric(hash, album_id) {
+    return axios({
+        method: 'get',
+        url: 'https://wwwapi.kugou.com/yy/index.php',
+        params: { r: play / getdata, hash, album_id },
+    })
+}
+/*************************上面是酷狗音乐API************************/
+
 export function apiRegister(user_id, sp) {
     return axios({
         url: 'api/register',
