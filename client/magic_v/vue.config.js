fe1d14b8a11e236b3f0c7f61c10023fdc2bc068f
colorthoro <dream.0112@qq.com>
2022-10-23 21:34:24 +0800

    按需自动导入elementui组件，如何设置为异步呢？

--- client/magic_v/vue.config.js
+++ client/magic_v/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
@@ -11,5 +14,15 @@ module.exports = defineConfig({
         }
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
   }
 })
