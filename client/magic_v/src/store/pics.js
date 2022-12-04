badbd51d03e3f19eb816f86976c85028a9d84569
colorthoro <dream.0112@qq.com>
2022-12-04 22:16:44 +0800

    pics 优化：不递交重复url，默认url冷处理

--- client/magic_v/src/store/pics.js
+++ client/magic_v/src/store/pics.js
@@ -4,9 +4,10 @@ export default defineStore('pics', {  // 避免重复创建歌曲图片对象
     state: () => ({
         pics: new Map(),  // picObjId -> {urlP, cnt} 
         url2id: new Map(), // url -> picObjId
-        defaultPicUrl: require("@/assets/background.png") + '?picId=',
+        defaultPicUrl: require("@/assets/background.png"),
+        defaultIds: [],
         nextId: 1,
-        cacheSize: 50,
+        cacheSize: 5,
     }),
     getters: {
         picInfo: (state) => (id, add = true) => {
@@ -25,18 +26,28 @@ export default defineStore('pics', {  // 避免重复创建歌曲图片对象
             if (!urlP) {
                 song.picId = this.nextId++;
                 urlP = song.fetchPicture().then(blob => {
-                    let url = blob ? URL.createObjectURL(blob) :
-                        this.defaultPicUrl + song.picId;
-                    this.url2id.set(url, song.picId);
+                    let url;
+                    if (blob) {
+                        url = URL.createObjectURL(blob);
+                        this.url2id.set(url, song.picId);
+                    } else {
+                        url = this.defaultPicUrl;
+                        this.defaultIds.push(song.picId);
+                    }
+                    console.log(song.picId, 'picUrl Promise fullfilled with', url);
                     return url;
                 });
-                this.pics.set(song.picId, { urlP, cnt: 1 });
+                this.pics.set(song.picId, { urlP, cnt: 1 });  // 若urlP还没有解决，则还没有占用该张图片
                 this.clear();
             }
             urlP.then(url => {
-                if (rId === reciver.id) reciver.url = url;  // reciver被弃用前把id删除，就不会再进行赋值
+                console.log(url);
+                if (rId === reciver.id) {  // reciver被弃用前把id删除，就不会再进行赋值
+                    if (reciver.url === url) return; // 若reciver原有一样的url，则不再递交url
+                    reciver.url = url;
+                }
                 else console.log('该图片接收者', reciver.id ? '已经转而请求其它图片了' : '已弃用');
-            })
+            });
             return urlP;
         },
         losePicUrl(url) {
@@ -45,16 +56,28 @@ export default defineStore('pics', {  // 避免重复创建歌曲图片对象
             let pic = this.picInfo(id, false);
             pic.cnt--;
         },
-        clear() {
+        async clear() {
             if (this.pics.size > this.cacheSize) {
-                for (let entry of this.url2id.entries()) {
-                    let url = entry[0], id = entry[1];
-                    console.log(url, id);
-                    if (this.picInfo(id, false).cnt === 0) {
+                console.log('start to clear unusing pics');
+                let k = this.defaultIds.length;
+                if (this.defaultIds.length) {
+                    this.defaultIds.forEach(id => {
+                        this.pics.delete(id);
+                    });
+                    this.defaultIds = [];
+                    console.log('cleared useless occupations');
+                    return;
+                }
+                for (let entry of this.pics.entries()) {
+                    let id = entry[0], info = entry[1];
+                    (info.cnt === 0) && await info.urlP.then(url => {
                         URL.revokeObjectURL(url);
+                        console.log(id, info, 'cleared');
                         this.pics.delete(id);
                         this.url2id.delete(url);
-                    }
+                        k++;
+                    });
+                    if (k > this.cacheSize / 2) return;
                 }
             }
         }
